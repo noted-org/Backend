231e7d4ebe3f027833c935284be4ecc1cc398c58
Marc Binder <marc@mail.bgfxc4.de>
2025-04-21 22:57:30 +0200

    add dummy summarize endpoint

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,16 +1,16 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  ParseIntPipe,
-  NotFoundException,
-  UseGuards,
-  Req,
-  ForbiddenException,
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	ParseIntPipe,
+	NotFoundException,
+	UseGuards,
+	Req,
+	ForbiddenException,
 } from "@nestjs/common";
 import { ZodValidationPipe } from "src/validation.pipe";
 import { ApiBearerAuth } from "@nestjs/swagger";
@@ -23,133 +23,158 @@ import { TagsService } from "src/tags/tags.service";
 
 @Controller("notes")
 export class NotesController {
-  constructor(
-    private readonly notesService: NotesService,
-    private readonly tagsService: TagsService,
-  ) { }
+	constructor(
+		private readonly notesService: NotesService,
+		private readonly tagsService: TagsService,
+	) { }
 
-  @Get()
-  findAll() {
-    return this.notesService.findAll();
-  }
+	@Get()
+	findAll() {
+		return this.notesService.findAll();
+	}
 
-  @Get(":id")
-  async findOne(@Param("id", ParseIntPipe) id: number) {
-    const answ = await this.notesService.findOne(id);
+	@Get(":id")
+	async findOne(@Param("id", ParseIntPipe) id: number) {
+		const answ = await this.notesService.findOne(id);
 
-    if (answ) {
-      return answ;
-    }
+		if (answ) {
+			return answ;
+		}
 
-    throw new NotFoundException();
-  }
+		throw new NotFoundException();
+	}
 
-  @ApiBearerAuth()
-  @Post()
-  @UseGuards(AuthIdGuard)
-  create(
-    @Body(new ZodValidationPipe(createNoteSchema)) createNoteDto: CreateNoteDto,
-    @Req() request: Request,
-  ) {
-    return this.notesService.create(createNoteDto, request["user"]?.id);
-  }
+	@ApiBearerAuth()
+	@Post()
+	@UseGuards(AuthIdGuard)
+	create(
+		@Body(new ZodValidationPipe(createNoteSchema)) createNoteDto: CreateNoteDto,
+		@Req() request: Request,
+	) {
+		return this.notesService.create(createNoteDto, request["user"]?.id);
+	}
 
-  @ApiBearerAuth()
-  @Patch(":id")
-  async update(
-    @Param("id", ParseIntPipe) id: number,
-    @Body(new ZodValidationPipe(updateNoteSchema)) updateNoteDto: UpdateNoteDto,
-    @Req() request: Request,
-  ) {
-    const note = await this.notesService.findOne(id);
+	@ApiBearerAuth()
+	@Patch(":id")
+	async update(
+		@Param("id", ParseIntPipe) id: number,
+		@Body(new ZodValidationPipe(updateNoteSchema)) updateNoteDto: UpdateNoteDto,
+		@Req() request: Request,
+	) {
+		const note = await this.notesService.findOne(id);
 
-    if (!note) {
-      throw new NotFoundException("Note with this id does not exist");
-    }
+		if (!note) {
+			throw new NotFoundException("Note with this id does not exist");
+		}
 
-    if (note.author !== request["user"]?.id) {
-      throw new ForbiddenException("You cannot edit notes from other users");
-    }
+		if (note.author !== request["user"]?.id) {
+			throw new ForbiddenException("You cannot edit notes from other users");
+		}
 
-    const count = await this.notesService.update(id, updateNoteDto);
+		const count = await this.notesService.update(id, updateNoteDto);
 
-    if (count[0] === 1) {
-      return this.notesService.findOne(id);
-    }
+		if (count[0] === 1) {
+			return this.notesService.findOne(id);
+		}
 
-    throw new NotFoundException("The note was not found");
-  }
+		throw new NotFoundException("The note was not found");
+	}
 
-  @ApiBearerAuth()
-  @Delete(":id")
-  @UseGuards(AuthIdGuard)
-  async remove(@Param("id", ParseIntPipe) id: number, @Req() request: Request) {
-    const note = await this.notesService.findOne(id);
+	@ApiBearerAuth()
+	@Delete(":id")
+	@UseGuards(AuthIdGuard)
+	async remove(@Param("id", ParseIntPipe) id: number, @Req() request: Request) {
+		const note = await this.notesService.findOne(id);
 
-    if (!note) {
-      throw new NotFoundException("Note with this id does not exist");
-    }
+		if (!note) {
+			throw new NotFoundException("Note with this id does not exist");
+		}
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    if (note?.dataValues?.author !== request["user"]?.id) {
-      throw new ForbiddenException("You cannot delete notes from other users");
-    }
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+		if (note?.dataValues?.author !== request["user"]?.id) {
+			throw new ForbiddenException("You cannot delete notes from other users");
+		}
 
-    const ret = await this.notesService.remove(id);
+		const ret = await this.notesService.remove(id);
 
-    if (ret === 1) {
-      return;
-    }
+		if (ret === 1) {
+			return;
+		}
 
-    throw new NotFoundException("Note with this id does not exist");
-  }
+		throw new NotFoundException("Note with this id does not exist");
+	}
 
-  @ApiBearerAuth()
-  @Post(":id/tags")
-  @UseGuards(AuthIdGuard)
-  async addTag(
-    @Param("id", ParseIntPipe) id: number,
-    @Body(new ZodValidationPipe(addTagsSchema)) addTagsDto: AddTagsDto,
-    @Req() request: Request,
-  ) {
-    const note = await this.notesService.findOne(id);
+	@ApiBearerAuth()
+	@Post(":id/tags")
+	@UseGuards(AuthIdGuard)
+	async addTag(
+		@Param("id", ParseIntPipe) id: number,
+		@Body(new ZodValidationPipe(addTagsSchema)) addTagsDto: AddTagsDto,
+		@Req() request: Request,
+	) {
+		const note = await this.notesService.findOne(id);
 
-    if (!note) {
-      throw new NotFoundException("Note with this id does not exist");
-    }
+		if (!note) {
+			throw new NotFoundException("Note with this id does not exist");
+		}
 
-    if (note?.dataValues?.author !== request["user"]?.id) {
-      throw new ForbiddenException("You cannot edit notes from other users");
-    }
+		if (note?.dataValues?.author !== request["user"]?.id) {
+			throw new ForbiddenException("You cannot edit notes from other users");
+		}
 
-    const tagsOfUser = await this.tagsService.findAll(request["user"]?.id);
-    if (addTagsDto.tags.filter((tag) => {
-      return !tagsOfUser.some((userTag) => userTag.id === tag);
-    }).length > 0) {
-      throw new NotFoundException("One or more tags do not exist for your user");
-    }
+		const tagsOfUser = await this.tagsService.findAll(request["user"]?.id);
+		if (addTagsDto.tags.filter((tag) => {
+			return !tagsOfUser.some((userTag) => userTag.id === tag);
+		}).length > 0) {
+			throw new NotFoundException("One or more tags do not exist for your user");
+		}
 
-    await this.notesService.addTags(id, addTagsDto.tags);
-  }
+		await this.notesService.addTags(id, addTagsDto.tags);
+	}
 
-  @ApiBearerAuth()
-  @Delete(":id/tags/:tagId")
-  @UseGuards(AuthIdGuard)
-  async removeTag(
-    @Param("id", ParseIntPipe) id: number,
-    @Param("tagId", ParseIntPipe) tagId: number,
-    @Req() request: Request,
-  ) {
-    const note = await this.notesService.findOne(id);
+	@ApiBearerAuth()
+	@Delete(":id/tags/:tagId")
+	@UseGuards(AuthIdGuard)
+	async removeTag(
+		@Param("id", ParseIntPipe) id: number,
+		@Param("tagId", ParseIntPipe) tagId: number,
+		@Req() request: Request,
+	) {
+		const note = await this.notesService.findOne(id);
 
-    if (!note) {
-      throw new NotFoundException("Note with this id does not exist");
-    }
+		if (!note) {
+			throw new NotFoundException("Note with this id does not exist");
+		}
 
-    if (note?.dataValues?.author !== request["user"]?.id) {
-      throw new ForbiddenException("You cannot edit notes from other users");
-    }
+		if (note?.dataValues?.author !== request["user"]?.id) {
+			throw new ForbiddenException("You cannot edit notes from other users");
+		}
 
-    await this.notesService.removeTag(id, tagId);
-  }
+		await this.notesService.removeTag(id, tagId);
+	}
+
+	@ApiBearerAuth()
+	@Get(":id/summarize")
+	@UseGuards(AuthIdGuard)
+	async summarize(
+		@Param("id", ParseIntPipe) id: number,
+		@Req() request: Request,
+	) {
+		const note = await this.notesService.findOne(id);
+
+		if (!note) {
+			throw new NotFoundException("Note with this id does not exist");
+		}
+
+		if (note?.dataValues?.author !== request["user"]?.id) {
+			throw new ForbiddenException("You cannot view notes from other users");
+		}
+
+		return `Placeholder summarize answer for note ${id} - ${note?.dataValues?.name}: 
+		lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+		incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+		exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+		reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+		cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+	}
 }
